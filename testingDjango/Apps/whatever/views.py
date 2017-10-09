856f4d9d384dfca298b1a7598867e97af717da36
JulietMind <32484424+JulietMind@users.noreply.github.com>
2017-10-09 15:01:55 +0200

    Blog funcional con Django conseguido

--- testingDjango/Apps/whatever/views.py
+++ testingDjango/Apps/whatever/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Temas
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'whatever/post_list.html', {})
+	post = Temas.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
+	return render(request, 'whatever/post_list.html', {'post': post})
--- testingDjango/settings.py
+++ testingDjango/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/C://xampp/htdocs/GitHubProjects/testingDjango/testingDjango/Apps/whatever/static/css/'
