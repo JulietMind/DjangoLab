b6d32ef0259eeddfe7e894834fb907a2bf5ebd98
JulietMind <32484424+JulietMind@users.noreply.github.com>
2017-10-10 14:28:59 +0200

    django.urls.exceptions.NoReverseMatch: Reverse for 'post_detail' with keyword arguments '{'pk': ''}' not found. 1 pattern(s) tried: ['post/(?P<pk>[0-9]+)/$']

--- testingDjango/Apps/whatever/views.py
+++ testingDjango/Apps/whatever/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Temas
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
+	from django.shortcuts import render, get_object_or_404
 	post = Temas.objects.filter(fecha_creacion__lte=timezone.now()).order_by('-fecha_creacion')
 	return render(request, 'whatever/post_list.html', {'post': post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Temas, pk=pk)
+    return render(request, 'whatever/post_detail.html', {'post': post})
